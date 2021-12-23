e97f4727ac79a0f5faf8a1a1da0ceb5e5c7b7d7c
Robert <robtom_5@mac.com>
2021-12-23 12:59:17 -0500

    Day 18 Done

--- 2021/18/18.py
+++ 2021/18/18.py
@@ -2,6 +2,51 @@ DEBUG = True
 import math
 
 
+def load_from_str(string_repr):
+    # Remove braces
+    contents = string_repr[1:-1]
+
+    # find mid point
+    half_point = 0
+    depth = 0
+    for i in range(len(contents)):
+        char = contents[i]
+        if char == "[":
+            depth += 1
+        elif char == "]":
+            depth -= 1
+        elif char == "," and depth == 0:
+            half_point = i
+            break
+    else:
+        raise Exception(f"Never found half point for {string_repr}")
+
+    first_half = contents[:half_point]
+    second_half = contents[half_point + 1 :]
+
+    try:
+        first_half = Pair(int(first_half), None)
+    except ValueError:
+        first_half = load_from_str(first_half)
+
+    try:
+        second_half = Pair(int(second_half), None)
+    except ValueError:
+        second_half = load_from_str(second_half)
+
+    return Pair.nest_pairs(first_half, second_half)
+
+
+def read_src():
+    src_file = "example.txt" if DEBUG else "src.txt"
+    with open(src_file, "r") as fh:
+        content = fh.readlines()
+
+    lines = [load_from_str(c.strip()) for c in content]
+
+    return lines
+
+
 class Pair:
     def __init__(self, left, right=None):
         self.left = left
@@ -15,6 +60,14 @@ class Pair:
     def set_as_right(self, parent):
         self.parent = parent
         self.is_left = False
+
+    def copy(self):
+        if self.is_root:
+            return Pair(self.left)
+        else:
+            left_copy = self.left.copy()
+            right_copy = self.right.copy()
+            return Pair.nest_pairs(left_copy, right_copy)
 
     @classmethod
     def nest_pairs(cls, left, right):
@@ -82,12 +135,34 @@ class Pair:
     def needs_reduction(self):
         return self.will_split or self.will_explode or self.child_need_reduction
 
-    def reduce(self):
-        # TODO: apply the explodes then splits in correct
-        # order. not just first come first serve
+    def ex(self):
+        if self.will_explode:
+            self.explode()
+            return True
+        elif not self.is_root:
 
-        # Loop through all node checking if any can explode,
-        # then if nothing explodes, split, and so on
+            l_exploded = self.left.ex()
+            if l_exploded:
+                return True
+            else:
+                return self.right.ex()
+        else:
+            return False
+
+    def sp(self):
+        if self.will_split:
+            self.split()
+            return True
+        elif not self.is_root:
+            l_split = self.left.sp()
+            if l_split:
+                return True
+            else:
+                return self.right.sp()
+        else:
+            return False
+
+    def reduce(self):
         if self.is_root:
             self.split()
         if self.needs_reduction:
@@ -103,9 +178,6 @@ class Pair:
             self.left, self.right = new_left, new_right
             self.left.set_as_left(self)
             self.right.set_as_right(self)
-
-            if self.needs_reduction:
-                self.left.reduce()
 
     def explode(self):
         if self.depth >= 4:
@@ -154,199 +226,56 @@ class SnailfishCalculator:
     @classmethod
     def add(cls, number1, number2):
         res = Pair.nest_pairs(number1, number2)
-        SnailfishCalculator.reduce(res)
 
-        i = 0
-        # while res.needs_reduction and i < 5:
-        #     # print(f"..{res}")
-        #     SnailfishCalculator.reduce(res)
-        #     i += 1
-        SnailfishCalculator.reduce(res)
-        # while res.needs_reduction:
-        #     print(f"..{res}")
-        #     res.reduce()
-        # else:
-        #     print(f"..{res}")
+        while res.needs_reduction:
+
+            # print(f"..{res}")
+            exploded = res.ex()
+            if exploded:
+                # print(f"X.{res}")
+                continue
+            else:
+                split = res.sp()
+                if split:
+                    # print(f"S.{res}")
+                    continue
+                else:
+                    pass
+        else:
+            # print(f"..{res}")
+            pass
         return res
-
-    @classmethod
-    def reduce(cls, pairs):
-
-        while cls.explode_first(pairs):
-            # Exploding
-            print(f"E.{pairs}")
-
-        while cls.split_first(pairs):
-            print(f"S.{pairs}")
-            while cls.explode_first(pairs):
-                print(f"..{pairs}")
-                pass
-
-        # TODO: apply the explodes then splits in correct
-        # order. not just first come first serve
-
-        # Loop through all node checking if any can explode,
-        # then if nothing explodes, split, and so on
-
-        # After an explode need to start looking from the left again
-
-        # Need to fix the finding of first to explode
-        pass
-
-    @classmethod
-    def explode_first(cls, pairs):
-        curr_node = pairs
-
-        while curr_node is not None:
-            # Explode if need to
-            if curr_node.will_explode:
-                curr_node.explode()
-                return True
-
-            if not curr_node.is_root:
-                if not curr_node.left.is_root:
-                    curr_node = curr_node.left
-                    continue
-                elif not curr_node.right.is_root:
-                    curr_node = curr_node.right
-                    continue
-
-            if curr_node.is_left:
-                curr_node = curr_node.parent.right
-            else:
-                while True:
-                    if curr_node.depth == 0:
-                        return False
-                    if curr_node.is_left:
-                        curr_node = curr_node.parent.right
-                    else:
-                        curr_node = curr_node.parent
-                else:
-                    # No mode nodes to examine
-                    curr_node = None
-                    break
-        return False
-
-    @classmethod
-    def split_first(cls, pairs):
-        curr_node = pairs
-        while curr_node is not None:
-            if curr_node.will_split:
-                curr_node.split()
-                print("split ", end="")
-                return True
-
-            if not curr_node.is_root:
-                if not curr_node.left.is_root:
-                    curr_node = curr_node.left
-                    continue
-                elif not curr_node.right.is_root:
-                    curr_node = curr_node.right
-                    continue
-
-            if curr_node.is_left:
-                curr_node = curr_node.parent.right
-            else:
-                # Go up until find next right
-                while True:
-                    if curr_node.depth == 0:
-                        return False
-                    if curr_node.is_left:
-                        curr_node = curr_node.parent.right
-                    else:
-                        curr_node = curr_node.parent
-                else:
-                    # No mode nodes to examine
-                    curr_node = None
-                    break
-        # print("done")
-        return False
-
-
-def load_from_str(string_repr):
-    # Remove braces
-    contents = string_repr[1:-1]
-
-    # find mid point
-    half_point = 0
-    depth = 0
-    for i in range(len(contents)):
-        char = contents[i]
-        if char == "[":
-            depth += 1
-        elif char == "]":
-            depth -= 1
-        elif char == "," and depth == 0:
-            half_point = i
-            break
-    else:
-        raise Exception(f"Never found half point for {string_repr}")
-
-    first_half = contents[:half_point]
-    second_half = contents[half_point + 1 :]
-
-    try:
-        first_half = Pair(int(first_half), None)
-    except ValueError:
-        first_half = load_from_str(first_half)
-
-    try:
-        second_half = Pair(int(second_half), None)
-    except ValueError:
-        second_half = load_from_str(second_half)
-
-    return Pair.nest_pairs(first_half, second_half)
-
-
-def read_src():
-    src_file = "example.txt" if DEBUG else "src.txt"
-    with open(src_file, "r") as fh:
-        content = fh.readlines()
-
-    lines = [load_from_str(c.strip()) for c in content]
-
-    return lines
 
 
 def task_1():
     content = read_src()
     rolling_sum = content.pop(0)
     for number in content:
-        print(f"  {rolling_sum}")
-        print(f"+ {number}")
+        # print(f"  {rolling_sum}")
+        # print(f"+ {number}")
         rolling_sum = SnailfishCalculator.add(rolling_sum, number)
-        print(f"= {rolling_sum}")
-        print()
-    # res = SnailfishCalculator.add(content[0], content[1])
-    print(rolling_sum)
-    # i = 0
-    # while i <= 5 and res.needs_reduction:
-    #     res.reduce()
-    #     print(res)
-    #     i += 1
-    print(f"task 1: ")
+        # print(f"= {rolling_sum}")
+        # print()
+
+    print(f"task 1: {rolling_sum.magnitude}")
 
 
 def task_2():
     content = read_src()
-    res = content.pop(0)
-    print(res)
-    i = 0
-    while i <= 5 and res.needs_reduction:
-        res.reduce()
-        print(res)
-        i += 1
-    # res = SnailfishCalculator.add(content[0], content[1])
-    print(f"task 2: ")
+
+    best_mag = 0
+    for a in content:
+        for b in content:
+            if a != b:
+                a_ = a.copy()
+                b_ = b.copy()
+                new_sum = SnailfishCalculator.add(a_, b_)
+                best_mag = max(new_sum.magnitude, best_mag)
+
+    print(f"task 2: {best_mag}")
 
 
 if __name__ == "__main__":
-    # DEBUG = False
+    DEBUG = False
     task_1()
-    # task_2()
-    # a = "[[[[0,7],4],[15,[0,13]]],[1,1]]"
-    # # a = "[[[[[9,8],1],2],3],4]"
-    # test_a = load_from_str(a)
-    # print(test_a)
-    # test_a.reduce()
-    # print(test_a)
+    task_2()
