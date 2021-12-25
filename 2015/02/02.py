40571fc4964f7547d2d1b2e1bad4ab828383f064
Robert <robtom_5@mac.com>
2021-12-25 17:59:53 -0500

    Day 02 2015

--- 2015/02/02.py
+++ 2015/02/02.py
@@ -1,26 +1,62 @@
 DEBUG = True
 
+import re
+
+present_regex = re.compile(r"^(?P<x>\d+)x(?P<y>\d+)x(?P<z>\d+)")
+
+
+def parse_line(line):
+    match = present_regex.match(line)
+    if match is not None:
+        x = int(match.group("x"))
+        y = int(match.group("y"))
+        z = int(match.group("z"))
+        return (x, y, z)
+
+
+def wrapping_paper_needed(dimension):
+    xy, xz, yz = sides(dimension)
+    min_sz = min([xy, xz, yz])
+    return 2 * (xy + xz + yz) + min_sz
+
+
+def sides(dimension):
+    x, y, z = dimension
+    xy = x * y
+    xz = x * z
+    yz = y * z
+    return xy, xz, yz
+
+
+def ribbon_needed(dimension):
+    x, y, z = dimension
+    dim_1, dim_2 = sorted([x, y, z])[0:2]
+    return 2 * (dim_1 + dim_2) + (x * y * z)
+
 
 def read_src():
     src_file = "example.txt" if DEBUG else "src.txt"
     with open(src_file, "r") as fh:
         content = fh.read().splitlines()
 
-    return content
+    dimensions = [parse_line(l) for l in content if parse_line(l)]
+
+    return dimensions
 
 
 def task_1():
-    content = read_src()
-    print(f"task 1: ")
+    dimensions = read_src()
+    total_wrapping_paper = sum([wrapping_paper_needed(d) for d in dimensions])
+    print(f"task 1: {total_wrapping_paper}")
 
 
 def task_2():
-    content = read_src()
-    print(f"task 2: ")
+    dimensions = read_src()
+    total_ribbon = sum([ribbon_needed(d) for d in dimensions])
+    print(f"task 2: {total_ribbon}")
 
 
 if __name__ == "__main__":
-    # DEBUG = False
+    DEBUG = False
     task_1()
     task_2()
-
