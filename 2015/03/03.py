54bfd0ba537485e297ce32e7019c9f49544cdff5
Robert <robtom_5@mac.com>
2021-12-25 18:13:06 -0500

    Day 03 2015

--- 2015/03/03.py
+++ 2015/03/03.py
@@ -1,26 +1,65 @@
 DEBUG = True
 
+import operator
+
 
 def read_src():
     src_file = "example.txt" if DEBUG else "src.txt"
     with open(src_file, "r") as fh:
         content = fh.read().splitlines()
 
-    return content
+    instructions = content[0]
+    return instructions
+
+
+def shift(base, delta):
+    return tuple(map(operator.add, base, delta))
+
+
+def move(instr, coords):
+    if instr == "^":
+        coord = shift(coords, (0, 1))
+    elif instr == ">":
+        coord = shift(coords, (1, 0))
+    elif instr == "<":
+        coord = shift(coords, (-1, 0))
+    elif instr == "v":
+        coord = shift(coords, (0, -1))
+    else:
+        raise Exception("Unknown Instruction")
+    return coord
 
 
 def task_1():
-    content = read_src()
-    print(f"task 1: ")
+    instructions = read_src()
+    visited_houses = set([(0, 0)])
+    curr_loc = (0, 0)
+    for ins in instructions:
+        curr_loc = move(ins, curr_loc)
+        visited_houses.add(curr_loc)
+
+    print(f"task 1: {len(visited_houses)} houses")
 
 
 def task_2():
-    content = read_src()
-    print(f"task 2: ")
+    instructions = read_src()
+    visited_houses = set([(0, 0)])
+    curr_loc = (0, 0)
+    robo_loc = (0, 0)
+    robo_active = False
+    for ins in instructions:
+        if robo_active:
+            robo_loc = move(ins, robo_loc)
+            visited_houses.add(robo_loc)
+        else:
+            curr_loc = move(ins, curr_loc)
+            visited_houses.add(curr_loc)
+        robo_active = not robo_active
+
+    print(f"task 2: {len(visited_houses)} houses")
 
 
 if __name__ == "__main__":
-    # DEBUG = False
+    DEBUG = False
     task_1()
     task_2()
-
