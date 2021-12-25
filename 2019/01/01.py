776470d90532bf87fb21ef8ac791e91332a70e1a
Robert <robtom_5@mac.com>
2021-12-25 10:11:17 -0500

    Started porting 2019

--- 2019/01/01.py
+++ 2019/01/01.py
@@ -1,26 +1,47 @@
 DEBUG = True
 
+import math
+
+
+def FuelToLaunch(mass):
+    return (math.floor(mass / 3)) - 2
+
+
+def AdvancedFuelToLaunch(mass):
+    # this is recursive
+
+    # fuel cost for the module, this is constant
+    fuel_mass = max(FuelToLaunch(mass), 0)
+
+    fuel_fuel = 0
+    if fuel_mass > 0:
+        fuel_fuel = AdvancedFuelToLaunch(fuel_mass)
+
+    return fuel_fuel + fuel_mass
+
 
 def read_src():
     src_file = "example.txt" if DEBUG else "src.txt"
     with open(src_file, "r") as fh:
         content = fh.read().splitlines()
 
-    return content
+    modules = [int(l) for l in content]
+    return modules
 
 
 def task_1():
-    content = read_src()
-    print(f"task 1: ")
+    modules = read_src()
+    total_fuel = sum([FuelToLaunch(m) for m in modules])
+    print(f"task 1: {total_fuel}")
 
 
 def task_2():
-    content = read_src()
-    print(f"task 2: ")
+    modules = read_src()
+    total_fuel = sum([AdvancedFuelToLaunch(m) for m in modules])
+    print(f"task 2: {total_fuel}")
 
 
 if __name__ == "__main__":
-    # DEBUG = False
+    DEBUG = False
     task_1()
     task_2()
-
