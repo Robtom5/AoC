60a3fef61b6fdea3d05e14253f78441539067d09
Robert <robtom_5@mac.com>
2021-12-26 14:52:54 -0500

    Day 4 2015

--- 2015/04/04.py
+++ 2015/04/04.py
@@ -1,26 +1,46 @@
 DEBUG = True
 
+import hashlib
+
 
 def read_src():
     src_file = "example.txt" if DEBUG else "src.txt"
     with open(src_file, "r") as fh:
         content = fh.read().splitlines()
 
-    return content
+    hash_root = content[0]
+    return hash_root
 
 
 def task_1():
-    content = read_src()
-    print(f"task 1: ")
+    hash_root = read_src()
+
+    starts_with_zeroes = False
+    i = 0
+    while not starts_with_zeroes:
+        i += 1
+        test_str = f"{hash_root}{i}"
+        hashed = hashlib.md5(test_str.encode())
+        starts_with_zeroes = hashed.hexdigest().startswith("00000")
+
+    print(f"task 1: {i}")
 
 
 def task_2():
-    content = read_src()
-    print(f"task 2: ")
+    hash_root = read_src()
+
+    starts_with_zeroes = False
+    i = 0
+    while not starts_with_zeroes:
+        i += 1
+        test_str = f"{hash_root}{i}"
+        hashed = hashlib.md5(test_str.encode())
+        starts_with_zeroes = hashed.hexdigest().startswith("000000")
+
+    print(f"task 2: {i}")
 
 
 if __name__ == "__main__":
-    # DEBUG = False
+    DEBUG = False
     task_1()
     task_2()
-
