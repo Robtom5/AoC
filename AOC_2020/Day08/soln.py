f1ad5b24bfa35c79ae1379458f89e61b6ee37933
Robert <robtom_5@mac.com>
2022-06-23 22:35:53 +0100

    2020 08 Ported and Optimised 5x Faster than previous soln

--- AOC_2020/Day08/soln.py
+++ AOC_2020/Day08/soln.py
@@ -9,18 +9,64 @@ def read_src():
     return content
 
 
+def loop_eval(instructions):
+    stuck_in_loop = False
+    accumulator = 0
+
+    pointer = 0
+    lines_hit = set()
+    while not stuck_in_loop and pointer < len(instructions):
+        if pointer in lines_hit:
+            stuck_in_loop = True
+        else:
+            lines_hit.add(pointer)
+            instr, val = instructions[pointer].split(" ")
+            if instr == "jmp":
+                pointer += int(val)
+                continue
+            elif instr == "acc":
+                accumulator += int(val)
+            pointer += 1
+    return stuck_in_loop, accumulator
+
+
 def task_1():
     content = read_src()
-    print(f"task 1: ")
+    _, acc = loop_eval(content)
+
+    print(f"task 1: {acc}")
 
 
 def task_2():
     content = read_src()
-    print(f"task 2: ")
+
+    error_cmds = {"nop", "jmp"}
+    potential_faults = [
+        index
+        for index in range(len(content))
+        if content[index].split(" ")[0] in error_cmds
+    ]
+    for index in potential_faults:
+        original = content[index]
+        cmd, val = original.split(" ")
+        if cmd == "jmp":
+            cmd = "nop"
+        else:
+            cmd = "jmp"
+        new = " ".join([cmd, val])
+        content[index] = new
+        fault, acc = loop_eval(content)
+
+        content[index] = original
+        if not fault:
+            break
+    else:
+        print("ERROR: No potential fix")
+
+    print(f"task 2: {acc}. Fix at {index}")
 
 
 if __name__ == "__main__":
-    # DEBUG = False
+    DEBUG = False
     task_1()
     task_2()
-
