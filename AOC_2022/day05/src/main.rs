ac78658d785a4eef34dcfddc4259c3655998b5b6
Rob Thomas <rob.thomas@raspberrypi.com>
2022-12-05 14:50:59 +0000

    Added skeleton for days 4 and 5

---
+++ AOC_2022/day05/src/main.rs
@@ -0,0 +1,14 @@
+use std::fs;
+
+fn main() {
+    let fp = if cfg!(debug_assertions) {
+        "data/example"
+    } else {
+        "data/input"
+    }.to_string();
+
+    let contents = fs::read_to_string(fp)
+        .expect("Should be able to read file");
+
+    
+}
