7f50f41d773deea53b6c225f093a9e0aa6d8c96e
Rob Thomas <rob.thomas@raspberrypi.com>
2022-12-06 14:24:45 +0000

    Day6 complete and added rust formatter

--- AOC_2022/day05/src/main.rs
+++ AOC_2022/day05/src/main.rs
@@ -1,45 +1,34 @@
-use std::fs;
 use regex::Regex;
+use std::fs;
 
-const ASCII_UPPER:[char; 26]= [
-    'A', 'B', 'C', 'D', 'E',
-    'F', 'G', 'H', 'I', 'J',
-    'K', 'L', 'M', 'N', 'O',
-    'P', 'Q', 'R', 'S', 'T',
-    'U', 'V', 'W', 'X', 'Y',
-    'Z',
-]; 
+const ASCII_UPPER: [char; 26] = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+];
 
+fn init_stacks(contents: &str) -> Vec<Vec<char>> {
+    let mut lines = contents.lines().peekable();
 
-
-fn init_stacks(contents: &str) -> Vec<Vec<char>>{
-    let mut lines = contents
-        .lines()
-        .peekable();
-
-    let first_line = lines
-        .peek()
-        .cloned()
-        .unwrap();
+    let first_line = lines.peek().cloned().unwrap();
 
     let number_of_stacks = (first_line.len() + 1) / 4;
     let mut layout: Vec<Vec<char>> = Vec::with_capacity(number_of_stacks as usize);
-    for _i in 0..number_of_stacks{
+    for _i in 0..number_of_stacks {
         layout.push(Vec::new());
     }
 
-    for line in lines{
+    for line in lines {
         let line_len = line.len();
-        for i in (0..line_len).step_by(4){
+        for i in (0..line_len).step_by(4) {
             let diff = match line_len - i {
                 x if x < 4 => x,
-                _ => 4, 
+                _ => 4,
             };
-            
-            let chars = &mut line[i..i+diff].chars();
+
+            let chars = &mut line[i..i + diff].chars();
             let box_char = chars.nth(1).unwrap();
-            let stack_id: usize = i /4;
-            match ASCII_UPPER.iter().any(|&x| x == box_char){  
+            let stack_id: usize = i / 4;
+            match ASCII_UPPER.iter().any(|&x| x == box_char) {
                 true => layout[stack_id].insert(0, box_char),
                 false => continue,
             };
@@ -49,27 +38,27 @@ fn init_stacks(contents: &str) -> Vec<Vec<char>>{
     return layout;
 }
 
-fn parse_instruction(line: &str) -> Result<(u16, usize, usize), &str>{
+fn parse_instruction(line: &str) -> Result<(u16, usize, usize), &str> {
     let re = Regex::new(r"^move (?P<volume>\d+) from (?P<src>\d+) to (?P<dst>\d+)$").unwrap();
-    return match re.is_match(line){
+    return match re.is_match(line) {
         true => Ok(split_cap(re.captures_iter(line))),
         false => Err("No valid instructions"),
-    }
-}   
+    };
+}
 
 fn split_cap(mut captures: regex::CaptureMatches) -> (u16, usize, usize) {
     let cap = captures.next().unwrap();
     let volume = &cap["volume"].parse::<u16>().unwrap();
     let src = &cap["src"].parse::<usize>().unwrap() - 1;
     let dst = &cap["dst"].parse::<usize>().unwrap() - 1;
-    return (*volume, src, dst)
+    return (*volume, src, dst);
 }
 
-fn apply_instructions(stacks: &mut Vec<Vec<char>>, contents: &str){
+fn apply_instructions(stacks: &mut Vec<Vec<char>>, contents: &str) {
     let lines = contents.lines();
 
     for line in lines {
-        let (volume, src, dst) = match parse_instruction(line){
+        let (volume, src, dst) = match parse_instruction(line) {
             Ok(n) => n,
             Err(_e) => continue,
         };
@@ -81,11 +70,11 @@ fn apply_instructions(stacks: &mut Vec<Vec<char>>, contents: &str){
     }
 }
 
-fn apply_instructions_2(stacks: &mut Vec<Vec<char>>, contents: &str){
+fn apply_instructions_2(stacks: &mut Vec<Vec<char>>, contents: &str) {
     let lines = contents.lines();
 
     for line in lines {
-        let (volume, src, dst) = match parse_instruction(line){
+        let (volume, src, dst) = match parse_instruction(line) {
             Ok(n) => n,
             Err(_e) => continue,
         };
@@ -94,22 +83,21 @@ fn apply_instructions_2(stacks: &mut Vec<Vec<char>>, contents: &str){
             let box_to_move = stacks[src].pop().unwrap();
             boxes_to_move.insert(0, box_to_move);
         }
-        for _box in boxes_to_move{
+        for _box in boxes_to_move {
             stacks[dst].push(_box);
         }
     }
 }
-
 
 fn main() {
     let fp = if cfg!(debug_assertions) {
         "data/example"
     } else {
         "data/input"
-    }.to_string();
+    }
+    .to_string();
 
-    let contents = fs::read_to_string(fp)
-        .expect("Should be able to read file");
+    let contents = fs::read_to_string(fp).expect("Should be able to read file");
 
     let mut stacks = init_stacks(&contents);
 
@@ -119,7 +107,7 @@ fn main() {
 
     for stack in stacks {
         let mut ch: char = '!';
-        for c in stack{
+        for c in stack {
             #[cfg(debug_assertions)]
             print!("{c} ");
             ch = c;
@@ -127,7 +115,7 @@ fn main() {
         final_str.push(ch);
         #[cfg(debug_assertions)]
         println!("")
-    }   
+    }
 
     println!("1st Str {final_str}");
 
@@ -138,7 +126,7 @@ fn main() {
 
     for stack in stacks_2 {
         let mut ch: char = '!';
-        for c in stack{
+        for c in stack {
             #[cfg(debug_assertions)]
             print!("{c} ");
             ch = c;
@@ -146,7 +134,7 @@ fn main() {
         final_str.push(ch);
         #[cfg(debug_assertions)]
         println!("")
-    }   
+    }
 
     println!("2nd Str {final_str}")
 }
---
+++ AOC_2022/day06/src/main.rs
@@ -0,0 +1,83 @@
+use std::collections::HashSet;
+use std::fs;
+
+fn incr_buff_pos(index: usize, buff_sz: usize) -> usize {
+    return match index {
+        n if n >= buff_sz - 1 => 0,
+        _ => index + 1,
+    };
+}
+
+fn decr_buff_pos(index: usize, buff_sz: usize) -> usize {
+    return match index {
+        n if n <= 0 => buff_sz - 1,
+        _ => index - 1,
+    };
+}
+
+trait Uniq {
+    fn is_unique(&self) -> bool;
+}
+
+impl Uniq for Vec<char> {
+    fn is_unique(&self) -> bool {
+        let sh: HashSet<&char> = HashSet::from_iter(self);
+        let hash_cap = sh.len();
+        let my_cap = self.len();
+
+        return hash_cap == my_cap;
+    }
+}
+
+fn find_start(contents: &str, buff_sz: usize) -> Result<(usize, String), &str> {
+    let mut rb: Vec<char> = vec!['!'; buff_sz];
+
+    let mut buff_index: usize = 0;
+    let mut char_index: usize = 0;
+
+    for _char in contents.chars() {
+        rb[buff_index] = _char;
+        char_index += 1;
+        buff_index = incr_buff_pos(buff_index, buff_sz);
+
+        if (char_index > buff_sz) && (rb.is_unique()) {
+            break;
+        }
+    }
+    let mut buffer_contents = "".to_owned();
+
+    for _ in 0..buff_sz {
+        buffer_contents.push(rb[buff_index]);
+        buff_index = decr_buff_pos(buff_index, buff_sz);
+    }
+
+    let message = buffer_contents.to_owned();
+    return Ok((char_index, message));
+    // return match char_index {
+    //     n if n < contents.len() => Ok((char_index, &message)),
+    //     _ => Err("No match found"),
+    // };
+}
+
+fn main() {
+    let fp = if cfg!(debug_assertions) {
+        "data/example"
+    } else {
+        "data/input"
+    }
+    .to_string();
+
+    let contents = fs::read_to_string(fp).expect("Should be able to read file");
+
+    let (index, packet) = match find_start(&contents, 4) {
+        Ok(n) => n,
+        Err(i) => panic!("{i}"),
+    };
+    println!("Packet {packet} starts at {index}");
+
+    let (index, message) = match find_start(&contents, 14) {
+        Ok(n) => n,
+        Err(i) => panic!("{i}"),
+    };
+    println!("Message {message} starts at {index}")
+}
