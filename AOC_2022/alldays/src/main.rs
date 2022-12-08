44f555c5b797aa4c828c46cc81bf462b6d2733b0
Rob Thomas <rob.thomas@raspberrypi.com>
2022-12-08 16:00:34 +0000

    Day 08 both stars

--- AOC_2022/alldays/src/main.rs
+++ AOC_2022/alldays/src/main.rs
@@ -1,5 +1,6 @@
 use itertools::Itertools;
 use regex::Regex;
+use std::collections::HashSet;
 use std::fs;
 
 fn day01(contents: &str) -> (String, String) {
@@ -348,6 +349,185 @@ fn day06(contents: &str) -> (String, String) {
     return (packet_index.to_string(), message_index.to_string());
 }
 
+fn day08(contents: &str) -> (String, String) {
+    let height: usize = contents.lines().count();
+    let width: usize = contents.len() / height - 1; // n lines with n carries
+
+    let mut visible = HashSet::new();
+
+    for row in 0..height {
+        // Count rowwise
+        let row_index = row * (width + 1);
+        let mut prev_height_l: i16 = -1;
+        let mut prev_height_r: i16 = -1;
+
+        for col in 0..width {
+            let l_index = row_index + col;
+            let l_height = match contents.get(l_index..l_index + 1) {
+                Some(n) => n.parse::<usize>().unwrap(),
+                None => 0,
+            };
+
+            let r_index = row_index + (width - 1 - col);
+            let r_height = match contents.get(r_index..r_index + 1) {
+                Some(n) => n.parse::<usize>().unwrap(),
+                None => 0,
+            };
+
+            match l_height {
+                h if h as i16 > prev_height_l => {
+                    visible.insert(l_index);
+                    prev_height_l = h as i16;
+                }
+                _ => {}
+            }
+
+            match r_height {
+                h if h as i16 > prev_height_r => {
+                    visible.insert(r_index);
+
+                    prev_height_r = h as i16;
+                }
+                _ => {}
+            }
+        }
+    }
+
+    for col in 0..width {
+        let mut prev_height_u: i16 = -1;
+        let mut prev_height_d: i16 = -1;
+
+        for row in 0..height {
+            let u_index = row * (width + 1) + col;
+
+            let u_height = match contents.get(u_index..u_index + 1) {
+                Some(n) => n.parse::<usize>().unwrap(),
+                None => 0,
+            };
+
+            let d_index = (height - row - 1) * (width + 1) + col;
+            let d_height = match contents.get(d_index..d_index + 1) {
+                Some(n) => n.parse::<usize>().unwrap(),
+                None => 0,
+            };
+
+            match u_height {
+                h if h as i16 > prev_height_u => {
+                    visible.insert(u_index);
+                    prev_height_u = h as i16;
+                }
+                _ => {}
+            }
+
+            match d_height {
+                h if h as i16 > prev_height_d => {
+                    visible.insert(d_index);
+
+                    prev_height_d = h as i16;
+                }
+                _ => {}
+            }
+        }
+    }
+    fn score_tree(
+        contents: &str,
+        (row, col): (usize, usize),
+        (height, width): (usize, usize),
+    ) -> usize {
+        let index = row * (width + 1) + col;
+        let tree_height = contents
+            .get(index..index + 1)
+            .expect("Invalid index")
+            .parse::<usize>()
+            .unwrap();
+
+        let mut visible_r = 0;
+        let mut visible_l = 0;
+        let mut visible_u = 0;
+        let mut visible_d = 0;
+
+        for x in (col + 1)..width {
+            let look_r = row * (width + 1) + x;
+
+            match contents
+                .get(look_r..look_r + 1)
+                .expect("Invalid index")
+                .parse::<usize>()
+                .unwrap()
+            {
+                n if n >= tree_height => {
+                    visible_r += 1;
+                    break;
+                }
+                _ => visible_r += 1,
+            }
+        }
+
+        for x in (0..col).rev() {
+            let look_l = row * (width + 1) + x;
+
+            match contents
+                .get(look_l..look_l + 1)
+                .expect("Invalid index")
+                .parse::<usize>()
+                .unwrap()
+            {
+                n if n >= tree_height => {
+                    visible_l += 1;
+                    break;
+                }
+                _ => visible_l += 1,
+            }
+        }
+
+        for y in (row + 1)..height {
+            let look_d = y * (width + 1) + col;
+
+            match contents
+                .get(look_d..look_d + 1)
+                .expect("Invalid index")
+                .parse::<usize>()
+                .unwrap()
+            {
+                n if n >= tree_height => {
+                    visible_d += 1;
+                    break;
+                }
+                _ => visible_d += 1,
+            }
+        }
+
+        for y in (0..row).rev() {
+            let look_u = y * (width + 1) + col;
+
+            match contents
+                .get(look_u..look_u + 1)
+                .expect("Invalid index")
+                .parse::<usize>()
+                .unwrap()
+            {
+                n if n >= tree_height => {
+                    visible_u += 1;
+                    break;
+                }
+                _ => visible_u += 1,
+            }
+        }
+        return visible_r * visible_l * visible_u * visible_d;
+    }
+
+    let mut best_score = 0;
+    for row in 0..height {
+        for col in 0..width {
+            match score_tree(&contents, (row, col), (height, width)) {
+                n if n > best_score => best_score = n,
+                _ => {}
+            }
+        }
+    }
+
+    return (visible.len().to_string(), best_score.to_string());
+}
 fn main() {
     for i in 1..25 {
         let fh = match cfg!(debug_assertions) {
@@ -367,6 +547,7 @@ fn main() {
             4 => day04(&contents),
             5 => day05(&contents),
             6 => day06(&contents),
+            8 => day08(&contents),
             _ => continue,
         };
 
--- AOC_2022/day07/src/main.rs
+++ AOC_2022/day07/src/main.rs
@@ -1,4 +1,121 @@
+use core::str::Lines;
 use std::fs;
+
+struct Node {
+    name: String,
+    // parent: Option<Box<Node>>,
+    file_size: u32,
+    children: Vec<Node>,
+}
+
+fn new_node(name: String) -> Node {
+    Node {
+        name: name,
+        // parent: None,
+        file_size: 0,
+        children: Vec::new(),
+    }
+}
+
+// trait
+
+// fn sum_small_contents(contents: &str, max: u32) -> u32 {
+//     return running_sum(contents.lines(), max);
+// }
+
+// fn running_sum(mut lines: Lines, max: u32) -> u32 {
+//     let mut dir_sum = 0;
+//     let line = lines.next().unwrap();
+//     let (word1, _word2) = line.split_once(' ').unwrap();
+//     println!("{max}");
+
+//     dir_sum += match word1.chars().next().unwrap() {
+//         '$' => 0,                       // Command
+//         'd' => running_sum(lines, max), // Directory
+//         _ => word1.parse::<u32>().unwrap(),
+//     };
+//     return match dir_sum {
+//         n if n < max => n,
+//         _ => 0,
+//     };
+// }
+fn load_folder(mut contents: &str) -> usize {
+    let mut folder_size = 0;
+    loop {
+        let next_line_break = match contents.find('\n') {
+            Some(n) => n,
+            None => break,
+        };
+        let line = &contents[..next_line_break];
+        contents = &contents[next_line_break..];
+
+        let words: Vec<&str> = line.split(' ').collect();
+        let mut word_iter = words.iter();
+
+        let word1 = word_iter.next().unwrap();
+
+        match *word1 {
+            "$" => {
+                let word2 = word_iter.next().unwrap();
+                match *word2 {
+                    "ls" => return 2,
+                    "cd" => {
+                        let word3 = word_iter.next().unwrap();
+                        match *word3 {
+                            ".." => 3, // Got back
+                            _ => load_folder(contents),
+                        }
+                    }
+                    _ => panic!("Unknown word {word2}"),
+                };
+            } // Command
+            "dir" => {}
+
+            _ => {
+                folder_size += word1.parse::<usize>().unwrap();
+            }
+        }
+    }
+    return folder_size;
+}
+
+fn part1(contents: &str) -> String {
+    // let sumofsmall = sum_small_contents(contents, 10000);
+    let mut lines = contents.lines();
+    lines.next(); // We dont need the first two lines
+    lines.next();
+    let root: Node = new_node("/".to_string());
+    let mut active_node = &root;
+
+    for line in lines {
+        let words: Vec<&str> = line.split(' ').collect();
+        let mut word_iter = words.iter();
+
+        let word1 = word_iter.next().unwrap();
+
+        match *word1 {
+            "$" => {
+                let word2 = word_iter.next().unwrap();
+                match *word2 {
+                    "ls" => {}
+                    "cd" => {
+                        let word3 = word_iter.next().unwrap();
+                        let &child_node = new_node(word3.to_string());
+
+                        active_node.children.push(child_node);
+                        active_node = &child_node;
+                    }
+                    _ => panic!("Unknown word {word2}"),
+                }
+            } // Command
+            "dir" => {}
+
+            _ => {}
+        }
+    }
+
+    return "sumofsmall".to_string();
+}
 
 fn main() {
     let fp = if cfg!(debug_assertions) {
@@ -9,4 +126,8 @@ fn main() {
     .to_string();
 
     let contents = fs::read_to_string(fp).expect("Should be able to read file");
+
+    let result1 = part1(&contents);
+
+    println!("Part 1 {result1}")
 }
