3ffaca9307863d083b814841289eb7c016e00453
Rob Thomas <rob.thomas@raspberrypi.com>
2022-12-09 16:41:57 +0000

    Day 07 completed

--- AOC_2022/day07/src/main.rs
+++ AOC_2022/day07/src/main.rs
@@ -1,8 +1,15 @@
+use std::cmp::min;
 use std::collections::HashSet;
 use std::fs;
 
-fn part1(contents: &str) -> String {
-    // let sumofsmall = sum_small_contents(contents, 10000);
+fn relevant_sz((name, sz): &(String, u64), start: &str) -> u64 {
+    match name.starts_with(start) {
+        true => return *sz,
+        false => return 0,
+    }
+}
+
+fn find_values(contents: &str) -> (String, String) {
     let mut path: String = "".to_owned();
     let mut files: Vec<(String, u64)> = Vec::new();
     let mut dirs: HashSet<String> = HashSet::new();
@@ -36,23 +43,29 @@ fn part1(contents: &str) -> String {
         }
     }
 
+    const MAX_SZ: u64 = 100000;
+    const TOTAL_DISK_SZ: u64 = 70000000;
+    const MIN_SZ: u64 = 30000000;
+
+    let already_available: u64 =
+        TOTAL_DISK_SZ - files.iter().map(|x| relevant_sz(x, "/")).sum::<u64>();
+    let size_to_delete = MIN_SZ - already_available;
+
+    assert!(MAX_SZ < size_to_delete); // We haven't accounted for this in the match statement so ensure if is is true
     let mut running_tot = 0;
+    let mut best_min = u64::MAX;
 
     for d in dirs {
-        fn relevant_sz((name, sz): &(String, u64), start: &str) -> u64 {
-            match name.starts_with(start) {
-                true => return *sz,
-                false => return 0,
-            }
-        }
         let tot_siz: u64 = files.iter().map(|x| relevant_sz(x, &d)).sum();
+
         match tot_siz {
-            n if n < 100000 => running_tot += n,
+            n if n < MAX_SZ => running_tot += n,
+            n if n > size_to_delete => best_min = min(best_min, n),
             _ => {}
         }
     }
 
-    return running_tot.to_string();
+    return (running_tot.to_string(), best_min.to_string());
 }
 
 fn main() {
@@ -65,7 +78,7 @@ fn main() {
 
     let contents = fs::read_to_string(fp).expect("Should be able to read file");
 
-    let result1 = part1(&contents);
+    let (result1, result2) = find_values(&contents);
 
-    println!("Part 1 {result1}")
+    println!("Part 1 {result1} Part 2 {result2}")
 }
