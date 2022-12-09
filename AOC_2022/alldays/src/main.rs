9bda3afd983f3a78db9da279d36ecbb9d970d5aa
Rob Thomas <rob.thomas@raspberrypi.com>
2022-12-09 15:47:11 +0000

    Added day09 to all days

--- AOC_2022/alldays/src/main.rs
+++ AOC_2022/alldays/src/main.rs
@@ -1,5 +1,8 @@
+use core::hash::Hash;
+use core::hash::Hasher;
 use itertools::Itertools;
 use regex::Regex;
+use std::cmp::max;
 use std::collections::HashSet;
 use std::fs;
 
@@ -526,7 +529,136 @@ fn day08(contents: &str) -> (String, String) {
         }
     }
 
-    return (visible.len().to_string(), best_score.to_string());
+    (visible.len().to_string(), best_score.to_string())
+}
+fn day09(contents: &str) -> (String, String) {
+    #[derive(Eq)]
+    struct Point {
+        x: i16,
+        y: i16,
+    }
+
+    impl PartialEq for Point {
+        fn eq(&self, other: &Self) -> bool {
+            (self.x == other.x) && (self.y == other.y)
+        }
+    }
+
+    impl Hash for Point {
+        fn hash<H: Hasher>(&self, state: &mut H) {
+            self.x.hash(state);
+            self.y.hash(state);
+        }
+    }
+
+    struct Rope {
+        head: Point,
+        tail: Point,
+    }
+
+    fn create_point() -> Point {
+        Point { x: 0, y: 0 }
+    }
+
+    fn create_point_from_coords(x: i16, y: i16) -> Point {
+        Point { x: x, y: y }
+    }
+
+    fn create_rope() -> Rope {
+        Rope {
+            head: create_point(),
+            tail: create_point(),
+        }
+    }
+
+    trait RopePhysics {
+        fn update_tail(&mut self) -> Point;
+        fn update_head(&mut self, pt: &Point);
+    }
+
+    trait Distance<Rhs = Self> {
+        fn distance_to(&self, other: &Rhs) -> i16;
+    }
+    impl RopePhysics for Rope {
+        fn update_tail(&mut self) -> Point {
+            let Point { x: hx, y: hy } = self.head;
+
+            let (dx, dy): (i16, i16) = match self.tail.distance_to(&self.head) {
+                _n if _n > 1 => {
+                    match &self.tail {
+                        Point { x, y } if x == &hx => (*x, y + (hy - y).signum()), // Same row
+                        Point { x, y } if y == &hy => (x + (hx - x).signum(), *y), //Same column,
+                        p => (p.x + (hx - p.x).signum(), p.y + (hy - p.y).signum()), // Point { x, y } => (*x, *y),
+                    }
+                }
+                _ => (self.tail.x, self.tail.y),
+            };
+            self.tail.x = dx;
+            self.tail.y = dy;
+            create_point_from_coords(dx, dy)
+        }
+
+        fn update_head(&mut self, pt: &Point) {
+            self.head.x = pt.x;
+            self.head.y = pt.y;
+        }
+    }
+
+    impl Distance for Point {
+        fn distance_to(&self, other: &Point) -> i16 {
+            let dx = (self.x - other.x).abs();
+            let dy = (self.y - other.y).abs();
+
+            return max(dx, dy);
+        }
+    }
+
+    fn rope_behaviour(contents: &str, rope_len: usize) -> String {
+        let mut visited: HashSet<Point> = HashSet::new();
+
+        visited.insert(create_point());
+
+        let mut ropes: Vec<Rope> = Vec::with_capacity(rope_len);
+        for _i in 0..rope_len {
+            ropes.push(create_rope());
+        }
+
+        for line in contents.lines() {
+            let (dir, dist) = match line.split_once(" ") {
+                Some(n) => n,
+                None => continue,
+            };
+            let dist_val = match dist.parse::<usize>() {
+                Ok(n) => n,
+                Err(_) => continue,
+            };
+
+            // let mut rope = &ropes[0];
+            // let mut tail;
+
+            for _i in 0..dist_val {
+                let mut rope_iter = ropes.iter_mut();
+                let mut rope = rope_iter.next().unwrap();
+                match dir {
+                    "R" => rope.head.x += 1,
+                    "L" => rope.head.x -= 1,
+                    "U" => rope.head.y += 1,
+                    "D" => rope.head.y -= 1,
+                    _ => panic!("Unknown direction"),
+                };
+
+                let mut last_tail = rope.update_tail();
+                for r in rope_iter {
+                    r.update_head(&last_tail);
+                    last_tail = r.update_tail();
+                }
+                visited.insert(last_tail);
+            }
+        }
+        visited.len().to_string()
+    }
+
+    (rope_behaviour(&contents, 1), rope_behaviour(&contents, 9))
 }
 fn main() {
     for i in 1..25 {
@@ -548,6 +680,7 @@ fn main() {
             5 => day05(&contents),
             6 => day06(&contents),
             8 => day08(&contents),
+            9 => day09(&contents),
             _ => continue,
         };
 
