c4f02c8f659b8353a7d7eac88759eb49eb2f1f4a
Rob Thomas <rob.thomas@raspberrypi.com>
2022-12-12 16:53:56 +0000

    Day 12 part 1 complete w/animation

--- AOC_2022/day12/src/main.rs
+++ AOC_2022/day12/src/main.rs
@@ -1,4 +1,118 @@
+use pathfinding::prelude::bfs;
 use std::fs;
+use std::{thread, time};
+
+const START_CHAR: char = 'S';
+const TARGET_CHAR: char = 'E';
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Pos(usize, usize);
+
+impl Pos {
+    fn successors(&self, grid: &Vec<Vec<u8>>) -> Vec<Pos> {
+        let &Pos(x, y) = self;
+        let pos_value = grid[y][x];
+        let (height, width) = (grid.len() - 1, grid[0].len() - 1);
+        let mut successors: Vec<Pos> = Vec::new();
+        let neighbors = match (x, y) {
+            (0, 0) => vec![(1, 0), (0, 1)],                      // TL
+            (0, y) if (y == height) => vec![(1, y), (0, y - 1)], // BL
+            (x, 0) if (x == width) => vec![(x - 1, 0), (x, 1)],  // TR
+            (x, y) if (x == width) && (y == height) => vec![(x - 1, y), (x, y - 1)], // BR
+            (0, y) => vec![(0, y + 1), (0, y - 1), (1, y)],      // L
+            (x, 0) => vec![(x + 1, 0), (x - 1, 0), (x, 1)],      // T
+            (x, y) if y == height => vec![(x + 1, y), (x - 1, y), (x, y - 1)], // B
+            (x, y) if x == width => vec![(x, y + 1), (x, y - 1), (x - 1, y)], // R
+            (x, y) => vec![(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)], // C
+        };
+        // println!("{dx} {dy} {height} {width}");
+        // println!("{x} {y}  {width} {height}  {:?}", neighbors);
+        for (dx, dy) in neighbors {
+            // let (X, Y) = (x as i16 + dx, y as i16 + dy);
+            print_pt(&Pos(dx, dy));
+            let s_v = grid[dy][dx];
+            match s_v as i16 - pos_value as i16 {
+                diff if diff <= 1 => {
+                    successors.push(Pos(dx as usize, dy as usize));
+                }
+                _ => {}
+            };
+        }
+        successors
+    }
+}
+
+fn part1(contents: &str) -> String {
+    let (map, src, dst) = load_map(&contents);
+
+    println!("{:?} {:?}", src, dst);
+
+    let (height, width) = (map.len() - 1, map[0].len() - 1);
+    print_grid(width, height);
+    let result = bfs(&src, |p| p.successors(&map), |p| *p == dst);
+    println!("{esc}[55E", esc = 27 as char);
+
+    return (result.expect("no path found").len() - 1).to_string();
+}
+
+fn print_grid(width: usize, height: usize) {
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // clear
+    let blankline = " ".repeat(width as usize) + "\n";
+    let blankgrid = blankline.repeat(height as usize);
+
+    println!("{blankgrid}");
+}
+
+fn print_pt(point: &Pos) {
+    let &Pos(x, y) = point;
+    print!("{esc}[H{esc}[{y};{x}H.{esc}[50E{x} {y}", esc = 27 as char);
+    thread::sleep(time::Duration::from_micros(100));
+    // thread::sleep(time::Duration::from_micros(10));
+}
+
+// fn printvisible(pt: Pos, width: i16, height: i16) {
+//     println!("{esc}[2J{esc}[1;1H", esc = 27 as char); // clear
+//     let blankline = ".".repeat(width as usize) + "\n";
+//     let mut blankgrid = blankline.repeat(height as usize);
+//     let origin = (height / 2) * (width + 1) + (width / 2);
+//     for pt in set {
+//         let x_val = pt.x;
+//         let y_val = pt.y;
+//         let value = (y_val * (width + 1) + x_val + origin) as usize;
+//         blankgrid.replace_range(value..(value + 1), "@");
+//     }
+//     println!("{blankgrid}");
+// }
+
+fn load_map(contents: &str) -> (Vec<Vec<u8>>, Pos, Pos) {
+    let height: usize = contents.lines().count();
+    let width: usize = contents.len() / height - 1; // n lines with n carries
+
+    // Final 2d array `&mut [&mut [_]]`
+    let mut grid = vec![vec![0u8; width]; height];
+    let mut src: Pos = Pos(0, 0);
+    let mut dst: Pos = Pos(0, 0);
+
+    for y in 0..height {
+        for x in 0..width {
+            let index = ((width + 1) * y) + x;
+            let value: char = match contents.chars().nth(index).unwrap() {
+                START_CHAR => {
+                    src = Pos(x, y);
+                    'a'
+                }
+                TARGET_CHAR => {
+                    dst = Pos(x, y);
+                    'z'
+                }
+                n => n,
+            };
+            let v_h = value.to_digit(36).unwrap() - 10;
+            grid[y][x] = v_h as u8;
+        }
+    }
+    (grid, src, dst)
+}
 
 fn main() {
     let fp = if cfg!(debug_assertions) {
@@ -9,4 +123,8 @@ fn main() {
     .to_string();
 
     let contents = fs::read_to_string(fp).expect("Should be able to read file");
+
+    let res1 = part1(&contents);
+
+    println!("Part 1 {res1}")
 }
