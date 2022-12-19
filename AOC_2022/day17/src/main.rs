a8007bc3391eb4a9da36add241bc699d599d671f
Rob Thomas <rob.thomas@raspberrypi.com>
2022-12-19 11:15:39 +0000

    Added skeletons for days 17,18,19

---
+++ AOC_2022/day17/src/main.rs
@@ -0,0 +1,44 @@
+use std::fs;
+
+const SEQ: &str = "####
+
+.#.
+###
+.#.
+
+..#
+..#
+###
+
+#
+#
+#
+#
+
+##
+##
+";
+
+fn part1(contents: &str) -> String {
+    "".to_string()
+}
+
+fn part2(contents: &str) -> String {
+    "".to_string()
+}
+
+fn main() {
+    let fp = if cfg!(debug_assertions) {
+        "data/example"
+    } else {
+        "data/input"
+    }
+    .to_string();
+
+    let contents = fs::read_to_string(fp).expect("Should be able to read file");
+
+    let res1 = part1(&contents);
+    let res2 = part2(&contents);
+
+    println!("Part 1 {res1} Part 2 {res2}")
+}
